279f9643dd82c2a6a55d0f42a9af391825b13767
world1919 <96011140+world1919@users.noreply.github.com>
2022-03-05 00:20:22 +0800

    Update TeeterIncentive.sol

--- contracts/TeeterIncentive.sol
+++ contracts/TeeterIncentive.sol
@@ -30,19 +30,10 @@ library TeeterIncentiveLibrary {
 }
 
 contract TeeterIncentive {
-    using SafeMath for uint256;
-    string public name;
-    string public symbol;
-    uint8 public constant decimals = 18;
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
-    mapping(address => uint256) public nonces;
     address public administrator;
-    uint16 public multiplier = 100;
-    //address public rewardToken = 0x55d398326f99059fF775485246999027B3197955;// BSC mainnet USDT
-    address public rewardToken = 0x6496d167C3c77d31D085CBB6B5396AF7686D98D7;// KOVAN TEST
-    uint256 public balanceLast;
+    uint16 public magnifyPrice = 1000000;//default 1000000 times
+    address public rewardToken;//stable coin
+    address public teeterCommunityToken;
     uint256 private unlocked = 1;
 
     modifier lock() {
@@ -52,36 +43,23 @@ contract TeeterIncentive {
         unlocked = 1;
     }    
 
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
     constructor() public {
         administrator = msg.sender;
-        symbol = "TC";
-        name = "TC";
-        //balanceLast = IERC20(rewardToken).balanceOf(address(this));
     }
 
-    //_multi is absolute value
-    function setMultiplier(uint16 _multi) public{
+    function setTeeterCommunityToken(address _token) public{
         require(msg.sender==administrator, "forbident");
-        require(_multi>0, "must > 0");
-        totalSupply = totalSupply.div(multiplier).mul(_multi);
-        multiplier = _multi;
+        teeterCommunityToken = _token;
     }
 
     function setRewardToken(address _token) public{
         require(msg.sender==administrator, "forbident");
         rewardToken = _token;
     }
-    
-    function getPrice() external view returns(uint256 _price){
-        //_price = (balanceLast.mul(10**decimals)).div(totalSupply);
-        _price = (balanceLast*(10**18))/totalSupply;
+
+    function setMagnifyPrice(uint256 _price) public{
+        require(msg.sender==administrator, "forbident");
+        magnifyPrice = _price;
     }
 
     function payback(address _token) public{
@@ -91,77 +69,13 @@ contract TeeterIncentive {
         }
     }    
 
-    function _mint(address to, uint256 value) internal {
-        totalSupply = totalSupply.add(value);
-        balanceOf[to] = balanceOf[to].add(value);
-        emit Transfer(address(0), to, value);
-    }
-
-    function mint() external lock returns(uint256 QTYAdded){
-        require(msg.sender==administrator, "forbident");
-        uint256 balCurr = TeeterIncentiveLibrary.convertTo18(rewardToken, IERC20(rewardToken).balanceOf(address(this)));
-        QTYAdded = balCurr.sub(balanceLast).mul(multiplier);
-        require(QTYAdded>0, "insufficient");
-        _mint(administrator, QTYAdded);
-        balanceLast = balCurr;
-    }
-
-    function _burn(address from, uint256 value) internal {
-        balanceOf[from] = balanceOf[from].sub(value);
-        totalSupply = totalSupply.sub(value);
-        emit Transfer(from, address(0), value);
-    }
-
-    function payReward(uint256 amt)public lock returns(uint256 rewarded){
-        TransferHelper.safeTransferFrom(address(this), msg.sender, address(this), amt);
-        //balanceLast*amt/totalSupply
-        rewarded = TeeterIncentiveLibrary.convert18ToOri(rewardToken, balanceLast.mul(amt).div(totalSupply));
+    function payReward(uint256 amtTeeterCommunityToken)public lock returns(uint256 rewarded){
+        TransferHelper.safeTransferFrom(teeterCommunityToken, msg.sender, address(this), amtTeeterCommunityToken);
+        amt18TeeterCT = TeeterIncentiveLibrary.convertTo18(teeterCommunityToken, amtTeeterCommunityToken);
+        amt18RewardT = TeeterIncentiveLibrary.convertTo18(rewardToken, SafeMath.div(amt18TeeterCT, magnifyPrice));
+        rewarded = TeeterIncentiveLibrary.convert18ToOri(rewardToken, amt18RewardT);
         TransferHelper.safeTransfer(rewardToken, msg.sender, rewarded);
-        _burn(address(this), amt);
-        balanceLast = TeeterIncentiveLibrary.convertTo18(rewardToken, IERC20(rewardToken).balanceOf(address(this)));
-    }
-
-    function _approve(
-        address owner,
-        address spender,
-        uint256 value
-    ) private {
-        allowance[owner][spender] = value;
-        emit Approval(owner, spender, value);
-    }
-
-    function _transfer(
-        address from,
-        address to,
-        uint256 value
-    ) private {
-        balanceOf[from] = balanceOf[from].sub(value);
-        balanceOf[to] = balanceOf[to].add(value);
-        emit Transfer(from, to, value);
-    }
-
-    function approve(address spender, uint256 value) external returns (bool) {
-        _approve(msg.sender, spender, value);
-        return true;
-    }
-
-    function transfer(address to, uint256 value) external returns (bool) {
-        _transfer(msg.sender, to, value);
-        return true;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) external returns (bool) {
-        if (allowance[from][msg.sender] != uint256(-1)) {
-            allowance[from][msg.sender] = allowance[from][msg.sender].sub(
-                value
-            );
-        }
-        _transfer(from, to, value);
-        return true;
+        IERC20(teeterCommunityToken).burn(amtTeeterCommunityToken)
     }
 
 }
