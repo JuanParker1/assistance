8079720f6e7e0351ded9d19bb7e1bd8bbb06713c
world1919 <jamesbondnow0@gmail.com>
2022-04-20 22:09:27 +0800

    add addMutiaddress

--- contracts/UAddressesRewardU.sol
+++ contracts/UAddressesRewardU.sol
@@ -61,6 +61,14 @@ contract UAddressesRewardU is IUAddressesRewardU{
       userQTY[_user] = _amt/(uint(10)**(IERC20(addrRewardToken).decimals()));
   }
   
+  function addMutiUserAddressAndAMT(address[] memory _users, uint256 _amt) public{
+    require(msg.sender==holder, "forbident");
+    for(uint16 i; i<_users.length; i++){
+        userAddress.push(_users[i]);
+        userAMT[_users[i]] = _amt;
+    }      
+  }
+  
   function setUserAddress(address[] memory _users) public{
     require(msg.sender==holder, "forbident");
     userAddress = _users;
@@ -100,28 +108,5 @@ contract UAddressesRewardU is IUAddressesRewardU{
 
   constructor() public{
         holder = msg.sender;
-        userAddress.push(0x69ba1c096A6E0C70592A03F17618d3b1E5B3164c);
-        userAddress.push(0xc02A97d90e3Cba663638EAE3008E2beA37ebcd3C);
-        userAddress.push(0x9e853BDA7b419be90E76316d00b783D1fFbe4189);
-        userAddress.push(0x872A596b1EC160a6a20d34B2B3a16E52dF149676);
-        userAddress.push(0x99E1B604E44Bf172281e3c74ef08E72Fa95cd23B);
-        userAddress.push(0x7C6b4590CD3Ce1484Ebc7189436b920420d20cc6);
-        userAddress.push(0x1C009C75089588BEf50Af8F6E08Ebbea8Cc6Bc17);
-        userAddress.push(0xAd3F0710A9dB58BdF1f1BDdDc6a1F471e154e016);
-        userAddress.push(0x397c5abd992713A97619b2B164Bae113bF1298F2);
-        userAddress.push(0xB97772700EC7BC4ce999dd22884455d4220F45d8);
-        userAddress.push(0x959506E7534901b707B6808917f69cbc96fA8c3d);
-
-        // userAMT[0X69ba1c096A6E0C70592A03F17618d3b1E5B3164c] = 10000000000000000000;
-        // userAMT[0Xc02A97d90e3Cba663638EAE3008E2beA37ebcd3C] = 10000000000000000000;
-        // userAMT[0X9e853BDA7b419be90E76316d00b783D1fFbe4189] = 10000000000000000000;
-        // userAMT[0X872A596b1EC160a6a20d34B2B3a16E52dF149676] = 10000000000000000000;
-        // userAMT[0X99E1B604E44Bf172281e3c74ef08E72Fa95cd23B] = 10000000000000000000;
-        // userAMT[0X7C6b4590CD3Ce1484Ebc7189436b920420d20cc6] = 10000000000000000000;
-        // userAMT[0X1C009C75089588BEf50Af8F6E08Ebbea8Cc6Bc17] = 10000000000000000000;
-        // userAMT[0XAd3F0710A9dB58BdF1f1BDdDc6a1F471e154e016] = 10000000000000000000;
-        // userAMT[0X397c5abd992713A97619b2B164Bae113bF1298F2] = 10000000000000000000;
-        // userAMT[0XB97772700EC7BC4ce999dd22884455d4220F45d8] = 10000000000000000000;
-        // userAMT[0X959506E7534901b707B6808917f69cbc96fA8c3d] = 10000000000000000000;
   } 
 }
